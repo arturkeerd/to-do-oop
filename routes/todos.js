8046e29c01f3b958aa921cded35873e063e83174
Artur Keerd <arturkeerd@gmail.com>
2024-12-11 12:08:18 +0200

    päev2 algus

---
+++ routes/todos.js
@@ -0,0 +1,7 @@
+import express, {Router} from 'express'
+import { TodoController } from '../controllers/todos.js'
+
+const router = Router()
+router.post('/new-todo', (req, res) => TodoController.createTodo(req, res))
+
+export default router
