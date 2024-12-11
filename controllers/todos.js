9c072c9331cab9e584a24412cf9d09142ef3692c
Artur Keerd <arturkeerd@gmail.com>
2024-12-11 12:56:12 +0200

    leht3

--- controllers/todos.js
+++ controllers/todos.js
@@ -5,13 +5,17 @@ class todoController {
         this.TODOS = []
     }
     createTodo(req,res){
+        console.log(req.body)
         const task = req.body.task
         const newTodo = new Todo(Math.random().toString(), task)
-    this.TODOS.push(newTodo)
-    res.json({
-        message: 'create d new todo object',
-        newTask: newTodo
-    }) 
+        this.TODOS.push(newTodo)
+        res.json({
+            message: 'create d new todo object',
+            newTask: newTodo
+        }) 
+    }
+    getTodos(req, res){
+        res.json({tasks: this.TODOS})
     }
 }
 
