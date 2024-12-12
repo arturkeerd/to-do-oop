77c0b92c5bb3c7313fd53921f573d6593179df49
Artur Keerd <arturkeerd@gmail.com>
2024-12-12 11:04:59 +0200

    read todos from file and write todos into file

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,23 +1,37 @@
+import { fileManager } from '../utils/files.js'
 import {Todo} from '../models/todo.js'
 
 class todoController {
     constructor() {
-        this.TODOS = []
+        this.initTodos()
     }
-    createTodo(req,res){
+    async createTodo(req,res){
         console.log(req.body)
         const task = req.body.task
         const newTodo = new Todo(Math.random().toString(), task)
         this.TODOS.push(newTodo)
+        await fileManager.writeFile('./data/todos.json', this.TODOS)
         res.json({
             message: 'created new todo object',
             newTask: newTodo
         }) 
     }
+
+    async initTodos() {
+        const todosData = await fileManager.readFile('./data/todos.json')
+        if(todosData !== null){
+            this.TODOS = todosData
+        } else {
+            this.TODOS = []
+        }
+    }
+
     getTodos(req, res){
         res.json({tasks: this.TODOS})
     }
-    updateTodo(req, res){
+    
+    async updateTodo(req, res){
+        try {
         console.log(req.body)
         const todoId = req.params.id
         const updatedTask = req.body.task
@@ -29,12 +43,21 @@ class todoController {
             throw new Error('Could not find todo!')
         }
         this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask)
+
+        await fileManager.writeFile('./data/todos.json', this.TODOS)
+
         res.json({
             message: 'Updated todo' ,
             updatedTask: this.TODOS[todoIndex]
         })
+        } catch (error) {
+            if (error.message.includes('todo')) {
+                console.log('update error => ', error.message)
+            }
+        }
     }
-    deleteTodo(req, res){
+    async deleteTodo(req, res){
+        try {
         const todoId = req.params.id
         const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
         if (todoIndex < 0) {
@@ -44,10 +67,18 @@ class todoController {
             throw new Error('Could not find todo!')
         }
         const deletedTask = this.TODOS.splice(todoIndex, 1)
+
+        await fileManager.writeFile('./data/todos.json', this.TODOS)
+
         res.json({
             message: 'Deleted todo successfully',
             deletedTask: deletedTask[0]
         })
+    } catch (error) {
+        if (error.message.includes('todo')) {
+            console.log('delete error => ', error.message)
+        }
+    }
     }
 }
 
